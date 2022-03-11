4a35c1da71d86cd8d68cc7d0da7650b4e0dbd94e
Andre-Moreno-Zamora <andre.moreno@atos.net>
2022-03-11 12:42:54 -0600

    Added spring boot project files

---
+++ src/test/java/com/example/datarestapi/DatarestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datarestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatarestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
